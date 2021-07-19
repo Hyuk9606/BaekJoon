1b942b1b16a960b750f7261711308f864a0d673c
LimHyuk <skdltm117@edu.ssafy.com>
2021-07-20 01:15:30 +0900

    210720_BJ-10757

---
+++ src/BJ_Practice/BJ_10757.java
@@ -0,0 +1,21 @@
+package BJ_Practice;
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class BJ_10757 {
+	
+	
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			BigInteger A = new BigInteger(st.nextToken());
+			BigInteger B = new BigInteger(st.nextToken());
+			A = A.add(B);
+			System.out.println(A.toString());
+			
+		}catch(Exception e) {}
+	}
+}
