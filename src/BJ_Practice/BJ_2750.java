11015573866ca61caab7b3e2e7b0c08b53f397e0
LimHyuk <skdltm117@gmail.com>
2021-07-25 01:13:26 +0900

    210725_BJ

--- src/BJ_Practice/BJ_2750.java
+++ src/BJ_Practice/BJ_2750.java
@@ -2,24 +2,25 @@ package BJ_Practice;
 
 import java.io.*;
 
-public class BJ_10989 {
+public class BJ_2750 {
+
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		try {
 			int N = Integer.parseInt(br.readLine());
 
-			int[] cnt = new int[10000];
+			int[] cnt = new int[2001];
 
 			for (int i = 0; i < N; i++)
-				cnt[Integer.parseInt(br.readLine())-1]++;
+				cnt[Integer.parseInt(br.readLine())+1000]++;
 
-			for (int i = 0; i < 10000; i++) {
+			for (int i = 0; i < 2001; i++) {
 				for (int j = 0; j < cnt[i]; j++) {
-					bw.write(Integer.valueOf(i+1) + "\n");
+					bw.write(Integer.valueOf(i-1000)+"\n");
 				}
 				bw.flush();
-
+				
 			}
 			bw.close();
 			br.close();
--- src/BJ_Practice/X_BJ_2751.java
+++ src/BJ_Practice/X_BJ_2751.java
@@ -2,21 +2,21 @@ package BJ_Practice;
 
 import java.io.*;
 
-public class BJ_10989 {
+public class X_BJ_2751 {
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		try {
 			int N = Integer.parseInt(br.readLine());
 
-			int[] cnt = new int[10000];
+			int[] cnt = new int[2000001];
 
 			for (int i = 0; i < N; i++)
-				cnt[Integer.parseInt(br.readLine())-1]++;
+				cnt[Integer.parseInt(br.readLine())+1000000]++;
 
-			for (int i = 0; i < 10000; i++) {
+			for (int i = 0; i < 2000001; i++) {
 				for (int j = 0; j < cnt[i]; j++) {
-					bw.write(Integer.valueOf(i+1) + "\n");
+					bw.write(Integer.valueOf(i-1000000) + "\n");
 				}
 				bw.flush();
 
