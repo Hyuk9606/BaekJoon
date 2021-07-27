e4b7be8d1bde5e77c5a62549d82abbb225fdfd59
LimHyuk <skdltm117@gmail.com>
2021-07-28 02:32:08 +0900

    210728_BJ-15927

---
+++ src/BJ_Practice/BJ_15927.java
@@ -0,0 +1,44 @@
+package BJ_Practice;
+
+import java.io.*;
+
+public class BJ_15927 {
+
+	static char[] c;
+	static int max = 0;
+
+	public static boolean check(int s, int e) {
+		if (s >= e)
+			return true;
+
+		else {
+			if (c[s] == c[e]) {
+				return check(s + 1, e - 1);
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		c = br.readLine().toCharArray();
+		max = c.length;
+		int s = 0;
+		int e = c.length - 1;
+		if (check(s, e)) {
+			max = e;
+			for (int i = 0; i <= max; i++)
+				if (c[0] == c[i]) {
+					if (i == c.length - 1) {
+						max = -1;
+						break;
+					}
+				}else break;
+
+		} else
+			max = c.length;
+		System.out.println(max);
+
+	}
+}
