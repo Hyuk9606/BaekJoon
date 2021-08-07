09c2c2500c040c6902bc20f4b9bc81e40b7fe2b7
LimHyuk <skdltm117@gmail.com>
2021-08-07 19:57:30 +0900

    210807_BJ-15651

---
+++ src/BJ_Practice/Silver/BJ_S3_15651.java
@@ -0,0 +1,43 @@
+package BJ_Practice.Silver;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_S3_15651 {
+	static int N;
+	static int M;
+	static int[] select;
+	static StringBuilder sb = new StringBuilder();
+	static void Combination(int cnt) {
+		if(cnt == M) {
+			for(int i= 0; i<M; i++) {
+			sb.append(select[i]).append(" ");	
+			}
+			sb.append("\n");
+			
+			return;
+		}
+		for(int i=1; i<=N; i++) {
+			
+			select[cnt] = i;
+			Combination(cnt+1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			select = new int[N+1];
+			Combination(0);
+			System.out.println(sb);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+}
