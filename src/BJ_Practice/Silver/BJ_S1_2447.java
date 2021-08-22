80c3e4539c4542f6094211b87cf8cccaf8ff36be
LimHyuk <skdltm117@gmail.com>
2021-08-22 13:35:12 +0900

    210822_BJ-2447(move)

---
+++ src/BJ_Practice/Silver/BJ_S1_2447.java
@@ -0,0 +1,34 @@
+package BJ_Practice.Silver;
+
+import java.util.*;
+
+public class BJ_S1_2447 {
+
+	static int N;
+	static StringBuilder sb = new StringBuilder();
+	static void star(int r, int c, int num) {
+
+		 if (((r / num) % 3 == 1 && (c / num) % 3 == 1)) {
+			sb.append(" ");
+		 }
+		 else {
+			 if(num/3 == 0)
+				 sb.append("*");
+			 else{
+				 star(r,c,num/3);
+			 }
+		 }
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++)
+				star(i, j, N);
+			sb.append("\n");
+		}
+		System.out.println(sb);
+
+	}
+}
