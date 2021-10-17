ed3f1890aba60ee26d95e298a79b89050f83f9fa
LimHyuk <skdltm117@gmail.com>
2021-10-17 16:25:31 +0900

    211017-BJ_S3_파도반_수열

---
+++ src/BJ_Practice/Silver/BJ_S3_9461_파도반_수열.java
@@ -0,0 +1,38 @@
+package BJ_Practice.Silver;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_S3_9461_파도반_수열 {
+	static int T;
+	static long[] dp;
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		T = Integer.parseInt(br.readLine());
+		int input[] = new int[T];
+		int N = 0;
+		for (int t = 1; t <= T; t++) {
+			input[t-1] = Integer.parseInt(br.readLine());
+			N = Math.max(input[t-1], N);
+		}
+		dp = new long[N+1];
+		for (int i = 0; i < T ; i++) {
+			System.out.println(waves(input[i]));
+		}
+	}
+	
+	//top-down
+	static long waves(int i) {
+		if(i < 4) return dp[i] = 1;
+		
+		else if(dp[i] == 0) {
+			return dp[i] = waves(i-2) + waves(i-3);
+		}
+		
+		return dp[i];
+	}
+}
