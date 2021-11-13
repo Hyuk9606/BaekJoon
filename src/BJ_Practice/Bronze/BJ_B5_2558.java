ddff791d8076e986a398250ea4334a12f5f38871
Lim Hyuk <skdltm117@gmail.com>
2021-11-13 20:23:04 +0900

    211113-BJ_B5_2558

---
+++ src/BJ_Practice/Bronze/BJ_B5_2558.java
@@ -0,0 +1,12 @@
+package BJ_Practice.Bronze;
+
+import java.util.*;
+
+public class BJ_B5_2558 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A+B);
+	}
+}
