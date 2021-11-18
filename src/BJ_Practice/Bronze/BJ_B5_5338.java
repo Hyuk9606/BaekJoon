daba3a81303c1e8ba4e5ecc991841a016c6cf5da
LimHyuk <skdltm117@gmail.com>
2021-11-18 23:53:38 +0900

    211118-BJ_B5_5338

---
+++ src/BJ_Practice/Bronze/BJ_B5_5338.java
@@ -0,0 +1,13 @@
+package BJ_Practice.Bronze;
+import java.io.*;
+
+public class BJ_B5_5338 {
+	public static void main(String[] args) throws IOException {
+
+		System.out.print("       _.-;;-._\r\n");
+		System.out.print("'-..-'|   ||   |\r\n");
+		System.out.print("'-..-'|_.-;;-._|\r\n");
+		System.out.print("'-..-'|   ||   |\r\n");
+		System.out.print("'-..-'|_.-''-._|");
+	}
+}
