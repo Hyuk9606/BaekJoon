ad0d555abd7eb4ae45562d86c8ac79d90b5eb5c4
LimHyuk <skdltm117@gmail.com>
2021-11-18 23:57:15 +0900

    ..

--- src/BJ_Practice/Bronze/BJ_B5_5338.java
+++ src/BJ_Practice/Bronze/BJ_B5_5338.java
@@ -4,10 +4,10 @@ import java.io.*;
 public class BJ_B5_5338 {
 	public static void main(String[] args) throws IOException {
 
-		System.out.print("       _.-;;-._\r\n");
-		System.out.print("'-..-'|   ||   |\r\n");
-		System.out.print("'-..-'|_.-;;-._|\r\n");
-		System.out.print("'-..-'|   ||   |\r\n");
-		System.out.print("'-..-'|_.-''-._|");
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
 	}
 }
