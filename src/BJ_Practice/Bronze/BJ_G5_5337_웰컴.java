aa101e7444e8e31364efb3755c5bda47c61b43d3
LimHyuk <skdltm117@gmail.com>
2021-12-28 19:09:14 +0900

    ..

---
+++ src/BJ_Practice/Bronze/BJ_G5_5337_웰컴.java
@@ -0,0 +1,20 @@
+package BJ_Practice.Bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_G5_5337_웰컴 {
+	
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		 String str = ".  .   .";
+		    System.out.println(str);
+		    str = "|  | _ | _. _ ._ _  _";
+		    System.out.println(str);
+		    str = "|/\\|(/.|(_.(_)[ | )(/.";
+		    System.out.println(str);
+	}
+	
+	
+}
