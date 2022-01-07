78fac9c95553e4dc162f651ee95eac5fc191fda8
Lim Hyuk <skdltm117@gmail.com>
2022-01-08 02:03:40 +0900

    220108-BJ_B2_시험 감독

---
+++ src/BJ_Practice/Bronze/BJ_B2_13458_시험_감독.java
@@ -0,0 +1,40 @@
+package BJ_Practice.Bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_B2_13458_시험_감독 {
+	static int N;
+	static int A[];
+	static int B, C;
+	static long ans = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		N = Integer.parseInt(br.readLine());
+		A = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < N; i++) {
+			A[i] -= B;
+			ans++;
+			if(A[i] <= 0) {
+				continue;
+			}
+			
+			ans += A[i]/C;
+			if(A[i] % C != 0) ans++;
+			
+		}
+		System.out.println(ans);
+
+	}
+}
