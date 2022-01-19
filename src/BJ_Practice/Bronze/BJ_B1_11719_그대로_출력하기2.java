cbbda92720a9f031c9dc6c6bc98617ca0acc1205
LimHyuk <skdltm117@gmail.com>
2022-01-19 19:40:46 +0900

    add : PS - BJ_B1_11719_그대로_출력하기2.java

---
+++ src/BJ_Practice/Bronze/BJ_B1_11719_그대로_출력하기2.java
@@ -0,0 +1,18 @@
+package BJ_Practice.Bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_B1_11719_그대로_출력하기2 {
+    static int N;
+    
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            System.out.println(sc.nextLine());
+        }
+        sc.close();
+    }
+}
