bbb652ae326c91962f3a3a1c1e20ffc3a06a1767
LimHyuk <skdltm117@gmail.com>
2022-02-04 23:34:04 +0900

    update: PS - BJ_G3_11967_불켜기ver2.java
    주석 추가

--- src/BJ_Practice/Gold/BJ_G3_11967_불켜기ver2.java
+++ src/BJ_Practice/Gold/BJ_G3_11967_불켜기ver2.java
@@ -11,8 +11,9 @@ public class BJ_G3_11967_불켜기ver2 {
     static int N, M;
     static int Ans;
     static int[][] deltas = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    static boolean[][] isShine;
-    static boolean[][] visited;
+    static boolean[][] isShine;     // 불이 켜져있는 방들
+    static boolean[][] visited;     // 불을 켠(스위치를 작동한) 방들
+    static boolean[][] isPassable;  // 이동할 수 있는 곳(불켠 방의 주변 방들)
     static Position[][] graph;
     
     static class Position {
@@ -37,8 +38,10 @@ public class BJ_G3_11967_불켜기ver2 {
         
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        
         isShine = new boolean[N + 1][N + 1];
         visited = new boolean[N + 1][N + 1];
+        isPassable = new boolean[N + 1][N + 1];
         graph = new Position[N + 1][N + 1];
         
         for (int i = 0; i < M; i++) {
@@ -48,10 +51,10 @@ public class BJ_G3_11967_불켜기ver2 {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             graph[x][y] = new Position(a, b, graph[x][y]);
-        }
+        }        // 입력 끝
         isShine[1][1] = true;
         visited[1][1] = true;
-        Ans++;
+        Ans++;    // (1,1)은 항상 불이 켜져있다.
         
         bfs();
         
@@ -60,43 +63,38 @@ public class BJ_G3_11967_불켜기ver2 {
     
     static void bfs() {
         Queue<Position> queue = new LinkedList<>();
-        boolean[][] check = new boolean[N + 1][N + 1];
         
         queue.offer(new Position(1, 1));
-        check[1][1] = true;
-        
+        isPassable[1][1] = true;
         
         while (!queue.isEmpty()) {
             Position cur = queue.poll();
+            // 불을 켤 수 있는 곳을 모두 켠다.
             for (Position temp = graph[cur.r][cur.c]; temp != null; temp = temp.link) {
-                if (!isShine[temp.r][temp.c]) {
+                if (!isShine[temp.r][temp.c]) {    // 불이 꺼져있었다면 불을 켜고 갯수를 추가한다.
                     isShine[temp.r][temp.c] = true;
                     Ans++;
                 }
-                if (check[temp.r][temp.c] && !visited[temp.r][temp.c]) {
+                if (isPassable[temp.r][temp.c] && !visited[temp.r][temp.c]) {    // 이동할 수 있는 위치이며, 스위치를 작동시키지 않았다면 queue에 넣는다.
                     queue.offer(new Position(temp.r, temp.c));
                     visited[temp.r][temp.c] = true;
                 }
                 
             }
             
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; i++) {    // 새로 방문한 방의 사방을 확인한다.
                 int nr = cur.r + deltas[i][0];
                 int nc = cur.c + deltas[i][1];
                 
                 if (isIn(nr, nc)) {
-                    if (!check[nr][nc] && isShine[nr][nc]) {
+                    isPassable[nr][nc] = true;    // 이 때 접근한 모든 방을 이동할 수 있는 방으로 만든다.
+                    if (!visited[nr][nc] && isShine[nr][nc]) {    // 불이 켜진 방을 만나면 queue에 넣는다. 이 때 들린 방 중 불 켜진 방이 있다면 queue에 넣는다.
                         queue.offer(new Position(nr, nc));
                         visited[nr][nc] = true;
                     }
-                    check[nr][nc] = true;
                 }
             }
         }
-    }
-    
-    static void turnOn(Position pos) {
-    
     }
     
     static boolean isIn(int r, int c) {
