1448768862a232ca5e2f53eaeb21e41b3a4f3c3e
LimHyuk <skdltm117@gmail.com>
2022-02-16 19:37:53 +0900

    add : BJ_B5_18108_1998년생인_내가_태국에서는_2541년생.java

---
+++ src/BJ_Practice/Bronze/BJ_B5_18108_1998년생인_내가_태국에서는_2541년생.java
@@ -0,0 +1,15 @@
+package BJ_Practice.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_B5_18108_1998년생인_내가_태국에서는_2541년생 {
+    static int N;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+        System.out.println(Integer.parseInt(br.readLine())-543);
+    }
+}
