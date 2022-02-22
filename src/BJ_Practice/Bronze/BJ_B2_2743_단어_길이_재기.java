337bcc2c13dad11fcc4b498aef3da71033f7b857
LimHyuk <skdltm117@gmail.com>
2022-02-22 19:21:56 +0900

    add : BJ_B2_2743_단어_길이_재기.java

---
+++ src/BJ_Practice/Bronze/BJ_B2_2743_단어_길이_재기.java
@@ -0,0 +1,11 @@
+package BJ_Practice.Bronze;
+
+import java.io.*;
+
+public class BJ_B2_2743_단어_길이_재기 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}
