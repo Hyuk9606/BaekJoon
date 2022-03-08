352d940c7612e06421e17a08837cabff5de35f9b
LimHyuk <skdltm117@gmail.com>
2022-03-08 22:48:20 +0900

    add : BJ_B5_15733_나는_누구인가.java

---
+++ src/BJ_Practice/Bronze/BJ_B5_15733_나는_누구인가.java
@@ -0,0 +1,10 @@
+package BJ_Practice.Bronze;
+
+import java.io.IOException;
+
+public class BJ_B5_15733_나는_누구인가 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("I'm Sexy");
+    }
+}
