137e73565784c8d818afd2594a78efdd32a68090
Lim Hyuk <87606735+Hyuk9606@users.noreply.github.com>
2022-06-13 18:50:48 +0900

    [Bronze V] Title: 개수 세기, Time: 76 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for(int i=0; i<N; i++){
+            if(Integer.parseInt(st.nextToken()) == v) answer++;
+        }
+        
+        System.out.println(answer);
+    }
+}
