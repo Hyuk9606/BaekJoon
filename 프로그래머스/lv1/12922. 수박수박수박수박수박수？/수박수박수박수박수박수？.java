7a7913262b19e28563788ba912e3d13dbf53ceaa
Lim Hyuk <87606735+Hyuk9606@users.noreply.github.com>
2022-06-13 21:00:33 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.07 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i< n/2; i++){
+            sb.append("수박");
+        }
+        
+        if(n%2 == 1) sb.append("수");
+        
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
