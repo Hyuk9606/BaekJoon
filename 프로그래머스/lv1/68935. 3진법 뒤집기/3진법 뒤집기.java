7a0687c42f95d8c2c8010a549c3bf5e8014b69bb
Lim Hyuk <87606735+Hyuk9606@users.noreply.github.com>
2022-06-17 22:00:52 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        sb.append(Integer.toString(n,3));
+        sb.reverse();
+        answer = Integer.parseInt(sb.toString(),3);
+        
+        return answer;
+    }
+}
