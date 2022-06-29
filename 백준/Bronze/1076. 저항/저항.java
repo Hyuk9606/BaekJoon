7101f2dc974487f4b09bae6abf7959d18a63f1df
Lim Hyuk <87606735+Hyuk9606@users.noreply.github.com>
2022-06-29 21:09:50 +0900

    [Bronze II] Title: 저항, Time: 72 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int T;
+	static String[] data = new String[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet",
+			"grey", "white" };
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		String[] strs = new String[3];
+		for (int i = 0; i < 3; i++) {
+			strs[i] = br.readLine();
+		}
+
+		String str = "";
+		int s = 0;
+
+		for (int i = 0; i < data.length; i++) {
+			if (strs[0].equals(data[i]))
+				str += String.valueOf(i);
+		}
+		for (int i = 0; i < data.length; i++) {
+			if (strs[1].equals(data[i]))
+				str += String.valueOf(i);
+		}
+		for (int i = 0; i < data.length; i++) {
+			if (strs[2].equals(data[i]))
+				s = i;
+		}
+		for (int i = 0; i < s; i++) {
+			str += "0";
+		}
+
+		System.out.println(Long.parseLong(str));
+	}
+}
