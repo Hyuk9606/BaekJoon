bb9011b9682530e4b82d1f7f6e3a05cec4e51f37
Lim Hyuk <87606735+Hyuk9606@users.noreply.github.com>
2022-12-24 21:31:11 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 76 ms, Memory: 11336 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int T;
+    static int n;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        T = Integer.parseInt(br.readLine());
+        
+        int dp[] = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        
+        for (int i = 4; i < 12; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        
+        while (T-- != 0) {
+            n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
